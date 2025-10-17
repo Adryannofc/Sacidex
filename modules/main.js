ba9230c4cc3ab7b1df8db2f143afc86940b475b9
Adryann Felix <adryannofc@gmail.com>
2025-10-17 19:16:23 -0300

    feat(data): 🗃️ adiciona dados mockados de Pokémons iniciais para demonstração

--- modules/main.js
+++ modules/main.js
@@ -1,11 +1,61 @@
 import { getPokemon } from "./js/api.js";
 import { createPokemonCard } from "./js/ui.js"
 
-async function main() {
-    const pokemon = await getPokemon(1) 
-    console.log(pokemon);
+const pokemons = [
+    {
+        id: 1,
+        name: "Bulbasaur",
+        types: [{ type: { name: "Grama" } }, { type: { name: "Venenoso" } }],
+        sprites: {
+            other: {
+                "official-artwork": {
+                    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                }
+            }
+        }
+    },
+    {
+        id: 4,
+        name: "Charmander",
+        types: [{ type: { name: "Fogo" } }],
+        sprites: {
+            other: {
+                "official-artwork": {
+                    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+                }
+            }
+        }
+    },
+    {
+        id: 7,
+        name: "Squirtle",
+        types: [{ type: { name: "Água" } }],
+        sprites: {
+            other: {
+                "official-artwork": {
+                    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+                }
+            }
+        }
+    },
+    {
+        id: 25,
+        name: "Pikachu",
+        types: [{ type: { name: "Elétrico" } }],
+        sprites: {
+            other: {
+                "official-artwork": {
+                    front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+                }
+            }
+        }
+    }
+];
 
-    createPokemonCard(pokemon)
+async function main() {
+    for(const item of pokemons){
+        createPokemonCard(item);
+    }
 }
 
 main();
