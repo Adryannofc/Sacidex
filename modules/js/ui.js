ffbc1be90a82b5795fee48828404d1a430c4b52f
Pedro Kleinhans <pedrokleinhans@gmail.com>
2025-10-26 22:57:58 -0300

    feat(css): animation favorite button

--- modules/js/ui.js
+++ modules/js/ui.js
@@ -19,8 +19,44 @@ export function createPokemonCard(pokemon) {
     const button = document.createElement("button");
     const imageButton = document.createElement("img")
     button.classList.add("card-favoriteButton");
-    
+    button.addEventListener('click', (e) => {
+        // Checa se o botão já está ativo
+        const isActive = button.classList.contains('active');
 
+        // Alterna a classe
+        button.classList.toggle('active');
+
+        // Só dispara partículas se ele **ficou ativo** agora
+        if (!isActive) {
+            // delay de 3 segundos antes das partículas
+            setTimeout(() => {
+                const numStars = 15;
+                for (let i = 0; i < numStars; i++) {
+                    const star = document.createElement('span');
+                    star.classList.add('star');
+
+                    // posição inicial
+
+
+                    // direção aleatória para cima
+                    const distance = 50 + Math.random() * 50;
+                    const xDir = (Math.random() - 0.5) * distance * 2; // pode ir para esquerda (-) ou direita (+)
+                    const yDir = -distance; // sempre sobe
+
+                    star.style.setProperty('--x', `${xDir}px`);
+                    star.style.setProperty('--y', `${yDir}px`);
+
+
+                    button.appendChild(star);
+
+                    // remove depois de 3s
+                    setTimeout(() => {
+                        star.remove();
+                    }, 1000);
+                }
+            }, 3300); // delay 3 segundos
+        }
+    });
     button.addEventListener('click', function (event) {
         // Impede o comportamento padrão do evento
         event.preventDefault();
@@ -28,51 +64,51 @@ export function createPokemonCard(pokemon) {
 
 
     button.addEventListener("click", (event) => {
-    event.stopPropagation(); // Impede que o clique no botão dispare o evento do card
+        event.stopPropagation(); // Impede que o clique no botão dispare o evento do card
 
-    const pageFavorite = localStorage.getItem("pageFavorite");
-    if (pageFavorite === 'true') {
-        localStorage.setItem("clickFavorite", 'true');
-    }
-
-    // Pega os favoritos salvos (ou cria um array vazio)
-    const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-
-    // Verifica se o Pokémon já está favoritado
-    const index = favoritos.findIndex(p => p.id === pokemon.id);
-
-    if (index >= 0) {
-        // Se já estiver, remove (desfavorita)
-        favoritos.splice(index, 1);
-        button.classList.remove('active');
-        button.classList.add('removeCapture');
-    } else {
-        // Se não estiver, adiciona
-        favoritos.push(pokemon);
-        button.classList.remove('removeCapture');
-        button.classList.add('active');
-    }
-
-    // Salva de volta no localStorage
-    localStorage.setItem("favoritos", JSON.stringify(favoritos));
-
-    function verificarFavoritePage() {
+        const pageFavorite = localStorage.getItem("pageFavorite");
         if (pageFavorite === 'true') {
-        const getFavoritos = localStorage.getItem("favoritos");
-        const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-        localStorage.removeItem("clickFavorite");
-        main(favoritos);
+            localStorage.setItem("clickFavorite", 'true');
         }
-    }
 
-    verificarFavoritePage();
+        // Pega os favoritos salvos (ou cria um array vazio)
+        const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+        // Verifica se o Pokémon já está favoritado
+        const index = favoritos.findIndex(p => p.id === pokemon.id);
+
+        if (index >= 0) {
+            // Se já estiver, remove (desfavorita)
+            favoritos.splice(index, 1);
+            button.classList.remove('active');
+            button.classList.add('removeCapture');
+        } else {
+            // Se não estiver, adiciona
+            favoritos.push(pokemon);
+            button.classList.remove('removeCapture');
+            button.classList.add('active');
+        }
+
+        // Salva de volta no localStorage
+        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+        function verificarFavoritePage() {
+            if (pageFavorite === 'true') {
+                const getFavoritos = localStorage.getItem("favoritos");
+                const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+                localStorage.removeItem("clickFavorite");
+                main(favoritos);
+            }
+        }
+
+        verificarFavoritePage();
 
 
-});
+    });
 
-// Retorna o css certo segundo o estado de favorito
-function verificarCaptura() {
-     const favoritos = JSON.parse(localStorage.getItem("favoritos"));
+    // Retorna o css certo segundo o estado de favorito
+    function verificarCaptura() {
+        const favoritos = JSON.parse(localStorage.getItem("favoritos"));
 
         // Verifica se o Pokémon já está favoritado
         const index = favoritos.findIndex(p => p.id === pokemon.id);
@@ -126,14 +162,14 @@ function verificarCaptura() {
 
     header.appendChild(name)
     header.appendChild(id);
-    
-    
+
+
 
 
     card.appendChild(header);
     card.appendChild(imageContainer);
     card.appendChild(types);
-    
+
 
 
 
